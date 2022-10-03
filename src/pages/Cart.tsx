8f0076d46a8ddb7b933d050a8d40767222cd0c65
thiethoang <vuthiet2k@gmail.com>
2022-10-03 17:00:32 +0700

    update cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Paper } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { deleteData } from "../apis";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
@@ -13,14 +13,44 @@ function Cart() {
   };
   const { cart, setCart } = useContext(CartContext);
   return (
-    <Box p={20} sx={{ backgroundColor: "#000" }}>
-      <Stack>
+    <Box
+      p={20}
+      sx={{
+        backgroundColor: "#000",
+      }}
+    >
+      <Stack sx={{ alignItems: "center" }}>
         {cart && cart.length > 0
           ? cart?.map((game: CartType) => {
               return (
-                <Paper key={game.id} sx={{m: '10px', height: '50px'}}>
+                <Paper
+                  key={game.id}
+                  sx={{
+                    m: "10px",
+                    minWidth: "650px",
+                    height: "50px",
+                    backgroundColor: "#262626",
+                    alignItems: "center",
+                    borderRadius: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    padding: "10px 20px",
+                    color: "#ccc",
+                    fontSize: "24px",
+                  }}
+                >
                   {game.name}
-                  <Button type="submit" onClick={() => handlerDelete(game.id)}>
+                  <Button
+                    type="submit"
+                    sx={{ color: "#fff", gap: "50px" }}
+                    onClick={() => handlerDelete(game.id)}
+                  >
+                    <Typography
+                      paragraph
+                      sx={{ color: "#999", margin: "0", fontSize: "16px" }}
+                    >
+                      ${game.money}
+                    </Typography>
                     X
                   </Button>
                 </Paper>
